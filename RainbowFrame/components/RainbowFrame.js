aae0ab9b763a10185f5fb9eb18b01cc85ebd9f48
AlexandraHessen <xl_energy@mail.ru>
2021-10-27 21:43:15 +0300

    plus comments

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -13,11 +13,12 @@ class RainbowFrame extends React.Component {
         function putIn(result, v, i, ) {
             return ( 
                 <div className = "RainbowFrame" style = {{border: "solid 8px " + v, padding: "8px"}} > 
+                {/* ОБЯЗАТЕЛЬНО СТАВИТЬ ПРОБЕЛ после 8px */}
                     {result} 
                 </div>
             )
         }
-        let colorFrame = (colorArr.reduce(putIn, this.props.children))
+        let colorFrame = (colorArr.reduce(putIn, this.props.children)) // this.props.children - начальное значение
 
         return <div> {colorFrame} </div>
     }
