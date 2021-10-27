51915fc016c3e45ebd379bf56afda33fd42f4642
AlexandraHessen <xl_energy@mail.ru>
2021-10-27 21:30:12 +0300

    RainbowFrame

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+    static propTypes = {
+        colors: PropTypes.array.isRequired
+    }
+
+    render() {
+        let colorArr = this.props.colors;
+        function putIn(result, v, i, ) {
+            return ( 
+                <div className = "RainbowFrame" style = {{border: "solid 8px " + v, padding: "8px"}} > 
+                    {result} 
+                </div>
+            )
+        }
+        let colorFrame = (colorArr.reduce(putIn, this.props.children))
+
+        return <div> {colorFrame} </div>
+    }
+}
+
+export default RainbowFrame
+
