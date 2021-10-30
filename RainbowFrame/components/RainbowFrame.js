8f11102120a368d606c4d1fe3a377ad8cf93bb9f
AlexandraHessen <xl_energy@mail.ru>
2021-10-30 20:33:31 +0300

    2 Способ forEach

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -10,17 +10,32 @@ class RainbowFrame extends React.Component {
 
     render() {
         let colorArr = this.props.colors;
-        function putIn(result, v, i, ) {
-            return ( 
-                <div className = "RainbowFrame" style = {{border: "solid 8px " + v, padding: "8px"}} > 
-                {/* ОБЯЗАТЕЛЬНО СТАВИТЬ ПРОБЕЛ после 8px */}
-                    {result} 
-                </div>
-            )
-        }
-        let colorFrame = (colorArr.reduce(putIn, this.props.children)) // this.props.children - начальное значение
 
-        return <div> {colorFrame} </div>
+// //--------------------- 1 Способ reduce ---------------------//
+//         function putIn(result, v, i, ) {
+//             return ( 
+//                 <div className = "RainbowFrame" style = {{border: "solid 8px " + v, padding: "8px"}} >   {/* ОБЯЗАТЕЛЬНО СТАВИТЬ ПРОБЕЛ после 8px */}                                                       
+//                     {result} 
+//                 </div>
+//             )
+//         }
+//         let colorFrame = (colorArr.reduce(putIn, this.props.children)) // this.props.children - начальное значение "Hello"
+//                                                                        // в reduce передается 1- массив, 2 - начальное значение
+//         return <div> {colorFrame} </div>
+
+//--------------------- 2 Способ forEach ---------------------//
+        let code=this.props.children //"Hello"
+        colorArr.forEach((color)=>{
+            code=<div className = "RainbowFrame" style = {{border: "solid 8px " + color, padding: "8px"}}>
+                    {code}
+                </div>
+            }
+        )
+        // ложим в переменную начальный текст, 
+        // затем в эту же перемнную каждую итерацию оборачиваем в div
+
+
+        return <div> {code} </div>
     }
 }
 
