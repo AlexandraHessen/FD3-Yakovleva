cf23bc0eb227d0c43c38e224281f7b6478272f1b
AlexandraHessen <xl_energy@mail.ru>
2021-10-30 21:46:57 +0300

    удаление лишнего эл в массиве

--- br2jsx/components/BR2JSX.js
+++ br2jsx/components/BR2JSX.js
@@ -4,14 +4,28 @@ import PropTypes from 'prop-types';
 import './BR2JSX.css';
 
 const BR2JSX = props =>{
-    let regExp = /\<[^<>]+\>/;
+    // let regExp = /\<[^<>]+\>/;
+    let regExp = /<br *\/?>/;
     let textArr= props.text.split(regExp);
     let text=[];
-    textArr.forEach((v, i)=>{ 
-        (!(i==textArr.length-1))
-        ?(text.push(v, <br key={i}/>))
-        :(text.push(v))
+
+//--------------------- УДАЛЕНИЕ ЛИШНЕГО (ПЕРВОГО-ПОСЛЕДНЕГО) ЭЛ В МАССИВЕ---------------------//
+    textArr.forEach((word, i)=>{ 
+        if(i){ // (i)?text.push(<br key={i}/>): null;
+            text.push(<br key={i}/>)
+        };         
+        text.push(word)
     })
+
+    // добавляем br вперед (не после а перед ) => получается лишний 0
+    // и ставить условие что при  i true с первого индекса т.е. не равно 0(false)
+
+    // textArr.forEach((word, i)=>{ 
+    //     (!(i==textArr.length-1))
+    //     ?(text.push(word, <br key={i}/>))
+    //     :(text.push(word))
+    // })
+
     return <div className="BR2JSX">{text}</div>
 }
 
