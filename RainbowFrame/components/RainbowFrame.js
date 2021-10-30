9d810541d140079162fa011164eea0b97ecf3159
AlexandraHessen <xl_energy@mail.ru>
2021-10-30 21:11:27 +0300

    1 Способ reduce

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -11,17 +11,17 @@ class RainbowFrame extends React.Component {
     render() {
         let colorArr = this.props.colors;
 
-// //--------------------- 1 Способ reduce ---------------------//
-//         function putIn(result, v, i, ) {
-//             return ( 
-//                 <div className = "RainbowFrame" style = {{border: "solid 8px " + v, padding: "8px"}} >   {/* ОБЯЗАТЕЛЬНО СТАВИТЬ ПРОБЕЛ после 8px */}                                                       
-//                     {result} 
-//                 </div>
-//             )
-//         }
-//         let colorFrame = (colorArr.reduce(putIn, this.props.children)) // this.props.children - начальное значение "Hello"
-//                                                                        // в reduce передается 1- массив, 2 - начальное значение
-//         return <div> {colorFrame} </div>
+//--------------------- 1 Способ reduce ---------------------//
+        function putIn(result, v, i, ) {
+            return ( 
+                <div className = "RainbowFrame" style = {{border: "solid 8px " + v, padding: "8px"}} >   {/* ОБЯЗАТЕЛЬНО СТАВИТЬ ПРОБЕЛ после 8px */}                                                       
+                    {result} 
+                </div>
+            )
+        }
+        let colorFrame = (colorArr.reduce(putIn, this.props.children)) // this.props.children - начальное значение "Hello"
+                                                                       // в reduce передается 1- массив, 2 - начальное значение
+        return <div> {colorFrame} </div>
 
 //--------------------- 2 Способ forEach ---------------------//
         // let code=this.props.children //"Hello"
@@ -31,24 +31,28 @@ class RainbowFrame extends React.Component {
         //         </div>
         //     }
         // )
-        // // ложим в переменную начальный текст, 
-        // // затем в эту же перемнную каждую итерацию оборачиваем в div
-
-
         // return <div> {code} </div>
 
+        //  ложим в переменную начальный текст, 
+        //  затем в эту же перемнную каждую итерацию оборачиваем в div
+
+
+
+
 //--------------------- 3 Способ Рекурсия ---------------------//
-        if (this.props.colors.length == 0) {
-            return this.props.children;
-        } else {
-            return ( 
-                <div className = "RainbowFrame" style = {{border: "solid 8px " + this.props.colors[0], padding: "8px"}} >   {/* ОБЯЗАТЕЛЬНО СТАВИТЬ ПРОБЕЛ после 8px */}                                                       
-                    <RainbowFrame colors={this.props.colors.slice(1)}>
-                        {this.props.children}
-                    </RainbowFrame>
-               </div>
-            )
-        }
+        // if (this.props.colors.length == 0) {
+        //     return this.props.children;
+        // } else {
+        //     return ( 
+        //         <div className = "RainbowFrame" style = {{border: "solid 8px " + this.props.colors[0], padding: "8px"}} >   {/* ОБЯЗАТЕЛЬНО СТАВИТЬ ПРОБЕЛ после 8px */}                                                       
+        //             <RainbowFrame colors={this.props.colors.slice(1)}>
+        //                 {this.props.children}
+        //             </RainbowFrame>
+        //        </div>
+        //     )
+        // }
+
+
         // 5. Создаем условие остановы - когда в массиве colors не будет цветов
         //    тогда отрисовываем Hello 
         // 
