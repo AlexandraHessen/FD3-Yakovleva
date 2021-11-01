dc5d024733c8830390396c04d6c53818510c9337
AlexandraHessen <xl_energy@mail.ru>
2021-11-01 16:25:38 +0300

    withRainbowFrame

---
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+let withRainbowFrame = colors=> Component => props=>{
+    let colorFrame = colors.reduce((startElm, color)=>{
+      return  <div className = "RainbowFrame" style = {{border: "solid 8px " + color, padding: "8px"}} >   {/* ОБЯЗАТЕЛЬНО СТАВИТЬ ПРОБЕЛ после 8px */}                                                       
+                {startElm} 
+               </div>
+    }, <Component {...props} />) // <Component {...props} /> - startElm - начальное значение "вышел, был сильный"
+
+    return <div> {colorFrame} </div>
+}
+
+export {withRainbowFrame}
+
+//--------------------- 2 Способ forEach ---------------------//
+
+// let withRainbowFrame = colors=> Component => props=>{
+//     let code=<Component {...props} />
+
+//     colors.forEach((color)=>{
+//         code=<div className = "RainbowFrame" style = {{border: "solid 8px " + color, padding: "8px"}}>
+//                 {code}
+//             </div>
+//         });
+
+//     return <div> {code} </div>
+// }
+
+
+
